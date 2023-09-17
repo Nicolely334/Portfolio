3e5bc787a5358934af27fc1adcb879e1553cb583
Nicole Ly <114376232+Nicolely334@users.noreply.github.com>
2023-09-16 20:02:00 -0700

    Delete assets/script.js

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menu-icon");
-    const dropdownContent = document.getElementById("menu-content");
-
-    menuIcon.addEventListener("click", function() {
-        dropdownContent.classList.toggle("show");
-    });
-});
